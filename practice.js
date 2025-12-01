80e18951213b538d1eec7eee88f425cd33a2206e
Bilal-Ahmed35 <bilalahmedshah35@gmail.com>
2025-12-01 15:13:08 +0500

    updated js

--- practice.js
+++ practice.js
@@ -1,28 +1,28 @@
-function add(a, b) {
-  return a + b;
-}
-console.log(`Adding 2+3 = ${add(2, 3)}`);
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log(`Adding 2+3 = ${add(2, 3)}`);
 
-const sub = (a, b) => a - b;
-console.log(`Subtracting 5-2 = ${sub(5, 2)}`);
+// const sub = (a, b) => a - b;
+// console.log(`Subtracting 5-2 = ${sub(5, 2)}`);
 
-const student = {
-  name: "Alice",
-  age: 20,
-  greet: () => {
-    console.log(`Hello, my name is ${this.name}`);
-  },
-};
-student.greet();
+// const student = {
+//   name: "Alice",
+//   age: 20,
+//   greet: () => {
+//     console.log(`Hello, my name is ${this.name}`);
+//   },
+// };
+// student.greet();
 
-const std = {
-  name: "Bilal",
-  age: 20,
-  greet: function () {
-    console.log(`Hello, my name is ${this.name}`);
-  },
-};
-std.greet();
+// const std = {
+//   name: "Bilal",
+//   age: 20,
+//   greet: function () {
+//     console.log(`Hello, my name is ${this.name}`);
+//   },
+// };
+// std.greet();
 
 const btn = document.querySelector("#plus");
 
@@ -36,6 +36,18 @@ btn.addEventListener("mouseleave", () => {
 //   return <button onclick={() => setCount(count + 1)}>{count}</button>;
 // }
 
-const arr1 = [1, 2, 3];
-const arr2 = [...arr1, 4, 5];
-console.log(arr2);
+// const arr1 = [1, 2, 3];
+// const arr2 = [...arr1, 4, 5];
+// console.log(arr2);
+
+const user = {
+  name: "Bilal",
+  company: {
+    name: "Beshax",
+    address: {
+      city: "Karachi",
+    },
+  },
+};
+console.log(user?.company?.address?.city);
+console.log(user?.ceo?.name);
